3730ec2c1bf2a5fee8d90321b535ddddb2d9df0a
moneerjarawneh <moneerjarawneh10@gmail.com>
2026-09-15 23:37:58 +0300

    Add first administrator setup

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\AuthController; use App\Http\Controllers\SaleController; use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/sales');
 Route::middleware('guest')->group(function(){Route::get('/login',[AuthController::class,'create'])->name('login');Route::post('/login',[AuthController::class,'store'])->name('login.store');});
+Route::middleware('guest')->group(function(){Route::get('/setup',[AuthController::class,'createFirstUser'])->name('setup');Route::post('/setup',[AuthController::class,'storeFirstUser'])->name('setup.store');});
 Route::post('/logout',[AuthController::class,'destroy'])->middleware('auth')->name('logout');
 Route::get('/sales',[SaleController::class,'index'])->middleware('auth')->name('sales.index');
 Route::post('/sales',[SaleController::class,'store'])->middleware('auth')->name('sales.store');
