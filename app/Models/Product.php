3b794e18fcbe5df9d9fedb44cb099365c3d5a523
moneerjarawneh <moneerjarawneh10@gmail.com>
2026-09-15 23:27:09 +0300

    Initial Laravel falafel sales system

---
+++ app/Models/Product.php
@@ -0,0 +1,3 @@
+<?php
+namespace App\Models; use Illuminate\Database\Eloquent\Model;
+class Product extends Model {protected $fillable=['category_id','name','price','is_active'];protected $casts=['price'=>'decimal:2','is_active'=>'boolean'];}
