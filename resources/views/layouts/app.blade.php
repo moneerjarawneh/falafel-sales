792a1aeb3215d86bd468c05390c4899e83775be0
moneerjarawneh <moneerjarawneh10@gmail.com>
2026-09-15 23:29:03 +0300

    Initial Laravel falafel sales system

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,1 @@
+<!doctype html><html lang="ar" dir="rtl"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>مبيعات الفلافل</title><link rel="stylesheet" href="{{ asset('css/app.css') }}"></head><body>@yield('content')</body></html>
